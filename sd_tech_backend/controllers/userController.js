6681699c5025caa3d4b058c2f062938e41a24ce0
Swans311 <DSwansey@email.neit.edu>
2022-08-30 14:16:59 -0400

    User controller & router update. Needs work still, needs Update function implemented.

--- sd_tech_backend/controllers/userController.js
+++ sd_tech_backend/controllers/userController.js
@@ -11,6 +11,41 @@ exports.createUser = (req,res) => {
     let sql = 'INSERT INTO sdtech.users (email, password, userType) VALUES ("'+data.email+'","'+data.password+'","'+data.userType+'");';
     let query = conn.query(sql, data, (err,results)=>{
         if(err) throw err;
-        res.send(JSON.stringify({'status':200, 'error':null, 'response':data}));
+        res.send(JSON.stringify({'status':200, 'error':null, 'response':results}));
+    });
+}
+exports.deleteUser = (req,res) => {
+    let data = {email:'undefined'}//req.body.email}
+    let sql = 'DELETE * FROM sdtech.users WHERE ("email" ="'+data.email+'");';
+    let query = conn.query(sql, data, (err,results)=>{
+        if(err) throw err;
+        res.send(JSON.stringify({'status':200, 'error':null, 'response':results}));
+    });
+}
+exports.getUserByEmail = (req,res)=>{
+    let data = {email:'email@email.com'}//req.body.email}
+    let sql = 'SELECT * FROM sdtech.users WHERE "email" ="'+data.email+'";';
+    let query = conn.query(sql, data, (err,results)=>{
+        if(err) throw err;
+        res.send(JSON.stringify({'status':200, 'error':null, 'response':results}));
+    });
+}
+
+exports.getUsers = (req,res) =>{
+    let data = {}
+    let sql = 'SELECT * FROM sdtech.users;';
+    let query = conn.query(sql, data, (err,results)=>{
+        if(err) throw err;
+        res.send(JSON.stringify({'status':200, 'error':null, 'response':results}));
+    });
+}
+
+exports.validateUser = (req,res) =>{
+    //let data={email:req.body.email, password:req.body.password}
+    let data={email:'email@email.com', password:'password'}
+    let sql = 'SELECT * FROM sdtech.users WHERE email = "'+data.email+'" AND password = "'+data.password+'";';
+    let query = conn.query(sql, data, (err,results)=>{
+        if(err) throw err;
+        res.send(JSON.stringify({'status':200, 'error':null, 'response':true}));
     });
 }
--- sd_tech_backend/routes/userRouter.js
+++ sd_tech_backend/routes/userRouter.js
@@ -6,9 +6,9 @@ const router = express.Router()
 
 router.post('/user', UserCtrl.createUser)
 // router.put('/user/:id', UserCtrl.updateUser)
-// router.delete('/user/:id', UserCtrl.deleteUser)
-// router.get('/user/:id', UserCtrl.getUserById)
-// router.get('/users',UserCtrl.getUsers)
-// router.post('/userValidate/:payload',UserCtrl.validateUser)
+router.delete('/user', UserCtrl.deleteUser)
+router.get('/userEmail', UserCtrl.getUserByEmail)
+router.get('/users',UserCtrl.getUsers)
+router.post('/userValidate',UserCtrl.validateUser)
 
 module.exports = router
